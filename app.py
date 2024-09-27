1714fc5de083cae4895a327ef2b67471a5b11d40
tvio <tvio@centrum.cz>
2024-09-27 14:26:19 +0200

    factory

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 import datetime
 from flask import Flask, render_template,request
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-entries = []
+    entries = []
 
-@app.route("/",methods=["GET","POST"])
-def home():
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formated_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entry_content, formated_date))
-    entries_with_date = [
-        (
-            entry[0],entry[1],datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
-        )
-        for entry in entries
-    ]
-    
-    return render_template("home.html",entries=entries_with_date)
+    @app.route("/",methods=["GET","POST"])
+    def home():
+            if request.method == "POST":
+                entry_content = request.form.get("content")
+                formated_date = datetime.datetime.today().strftime("%Y-%m-%d")
+                entries.append((entry_content, formated_date))
+            entries_with_date = [
+                (
+                entry[0],entry[1],datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+                )
+            for entry in entries
+            ]
+            return render_template("home.html",entries=entries_with_date)
+    return app
